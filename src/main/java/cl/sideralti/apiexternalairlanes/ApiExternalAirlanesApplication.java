14132e06748ee822189ba4b23c58f6f8015c6206
dev-gambits <emmanuel.nieto.munoz@ciisa.cl>
2023-07-11 22:47:55 -0400

    Initial commit

---
+++ src/main/java/cl/sideralti/apiexternalairlanes/ApiExternalAirlanesApplication.java
@@ -0,0 +1,13 @@
+package cl.sideralti.apiexternalairlanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExternalAirlanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExternalAirlanesApplication.class, args);
+	}
+
+}
